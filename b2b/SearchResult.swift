63f78dd95700883637bbd6bb0f42012ba8fce0bc
Marco Gnecchi <marco.gnecchi@icloud.com>
2017-09-27 11:25:22 +0200

    Ho aggiunto la SerachBar

---
+++ b2b/SearchResult.swift
@@ -0,0 +1,9 @@
+
+
+class SearchResult {
+    var codiceSm = ""
+    var codiceGcc = ""
+    var descrizione = ""
+    var modello = ""
+    var marchio = ""
+}
--- b2b/SearchViewController.swift
+++ b2b/SearchViewController.swift
@@ -13,6 +13,8 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,58 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+}
 
+var searchResults = [SearchResult]()
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        
+        searchResults = []
+        for _ in 0...2 {
+            let searchResult = SearchResult()
+            
+            searchResult.codiceGcc = "0205055161"
+            searchResult.codiceSm = "0427318"
+            searchResult.descrizione = "LAVATRICE CF 8KG 1200G A+++ DISP BIG"
+            searchResult.modello = "WAK24268IT"
+            searchResult.marchio = "BOSCH"
+            
+            searchResults.append(searchResult)
+        }
+        tableView.reloadData()
+    }
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell:UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        }
+        
+        cell.textLabel!.text = searchResults[indexPath.row].codiceGcc
+        cell.detailTextLabel!.text = searchResults[indexPath.row].descrizione
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
